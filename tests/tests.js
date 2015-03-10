282d01b1b1932ac03d3298a67a361d32148a9c03
Carlos <joseka1234@gmail.com>
2015-03-10 16:32:32 +0000

    Agregados los tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,32 +1,33 @@
 var assert = chai.assert;
 
   suite('CSV', function() {
-    test('4,5\n4,5,6 = error', function() {
-      var texto = "4,5" + '\n' + 
+    test('4,5\n4,5,6 = con error', function() {
+      var texto = "4,5" + "\n" + 
                   "4,5,6";
       original.value = texto;
       calculate();
-      var texto2 = "<p>\n<table class=\"center\" id=\"result\">" +
-                  "<tr>" +
-                  "<td>" +
-                  "4" +
-                  "</td>" +
-                  "<td>" +
-                  "5" +
-                  "</td>" +
-                  "</tr>" +
-                  "<tr class=\"error\">" +
-                  "<td>" +
-                  "4" +
-                  "</td>" +
-                  "<td>" +
-                  "5" +
-                  "</td>" +
-                  "<td>" +
-                  "6" +
-                  "</td>" +
-                  "</tr>" +
-                  "</table>";
-      assert.deepEquals(finaltable.innerHTML, texto2);
+      var texto2 =  '<p>\n</p><table class="center" id="result">\n<tbody><tr>'+
+                    '                <td>4</td>'+
+                    '                                           <td>5</td>'+
+                    '                           </tr>\n<tr class="error">'+
+                    '                <td>4</td>'+
+                    '                                           <td>5</td>'+
+                    '                                           <td>6</td>'+
+                    '                           </tr>\n</tbody></table>';
+      assert.deepEqual(finaltable.innerHTML, texto2);
+    });
+    test('4,5\n4,5 = sin error', function() {
+      var texto = "4,5" + "\n" + 
+                  "4,5";
+      original.value = texto;
+      calculate();
+      var texto2 =  '<p>\n</p><table class="center" id="result">\n<tbody><tr>'+
+                    '                <td>4</td>'+
+                    '                                           <td>5</td>'+
+                    '                           </tr>\n<tr>'+
+                    '                <td>4</td>'+
+                    '                                           <td>5</td>'+
+                    '                           </tr>\n</tbody></table>';
+      assert.deepEqual(finaltable.innerHTML, texto2);
     });
   });
