3cb5a61b07af27eb52fd14e5b6a3d1c6e69625ec
Carlos <joseka1234@gmail.com>
2015-03-10 16:09:30 +0000

    Añadido server y open a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,20 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   minifyHTML = require('gulp-minify-html');
   minifyCSS = require('gulp-minify-css');
+var shell = require('gulp-shell');
+
+
+gulp.task('default', ['server']);
+
+// npm install supervisor -g
+gulp.task('server', function () {
+  return gulp.src('').pipe(shell([ 'node-supervisor app.js' ]));
+});
+
+gulp.task('open', function() {
+  return gulp.src('').
+           pipe(shell("open https://github.com/crguezl/how-jquery-works-tutorial/tree/getallparams"));
+});
 
 /*
 * Configuración de la tarea 'demo'
